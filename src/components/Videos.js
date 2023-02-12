cc8e4acc8190569f0f42ade523edee3801c48ccf
kamesh666 <mskkamesh4@gmail.com>
2023-02-12 20:23:54 +0530

    channelcard

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -3,11 +3,10 @@ import { Stack, Box } from "@mui/material";
 import { VideoCard, ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
-  console.log(videos);
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, idx) => (
-        <Box key={idx}>
+      {videos.map((item) => (
+        <Box key={item.title}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
